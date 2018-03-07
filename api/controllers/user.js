8f46c2c5497f691c6fd291be34e832b7ca6e12ed
luis <luis152288@gmail.com>
2018-03-07 18:50:53 -0400

    paginacion y actualizacion de usuarios

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,6 +1,8 @@
 		'use strict'
 	var bcrypt = require('bcrypt-nodejs');
 
+	var mongoosePaginate = require('mongoose-pagination');
+
 	var User = require('../models/user');
 
 	var jwt = require('../services/jwt');
@@ -17,6 +19,7 @@ function test(req, res){
 	});
 }
 
+//registro
 function saveUser(req, res) {
 	var params = req.body;
 	var user = new User();
@@ -35,7 +38,7 @@ function saveUser(req, res) {
 							{email: user.email},
 							{nick: user.nick}
 					]}).exec((err, user) => {
-						if(err) return res.status(500).send({message:'error en la peticion de usuarios'});
+						if(err) return res.status(500).send({message:'Error en la peticion de usuarios'});
 
 						if(users && users.length >= 1){
 							return res.status(200).send({message:'El usuario ya existe!!'});
@@ -44,12 +47,12 @@ function saveUser(req, res) {
 								user.password = hash;
 
 								user.save((err, userStored) => {
-									if(err) return res.status(500).send({message:'error al guardar usuario'});
+									if(err) return res.status(500).send({message:'Error al guardar usuario'});
 
 									if (userStored) {
 										res.status(200).send({user: userStored});
 									}else{
-										res.status(404).send({message:'no se ha registrado el usuario'});
+										res.status(404).send({message:'No se ha registrado el usuario'});
 									}
 								});
 							});
@@ -63,6 +66,7 @@ function saveUser(req, res) {
 
 }
 
+//login
 function loginUser(req, res){
 	var params = req.body;
 
@@ -100,9 +104,78 @@ function loginUser(req, res){
 	});
 }
 
+
+//conseguir datos de usuario
+
+function getUser(req, res) {
+	var userId = req.params.id;
+
+	User.findById(userId, (err, user) => {
+		if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+		if(!user) return res.status(404).send({message: 'Usuario no encontrado'});
+
+		return res.status(200).send({user});
+	});
+}
+
+// paginado de usurios
+
+function getUsers(req, res) {
+	var identity_user_id = req.user.sub;
+	var page = 1;
+
+
+	if (req.params.page) {
+		page = req.params.page;
+	}
+
+	var itemsPerPage = 5;
+
+	User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) =>{
+		if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+		if (!users) return res.status(404).send({message: 'No se encontro  ningun usuario'});
+	
+		return res.status(200).send({
+			users,
+			total,
+			page: Math.ceil(total/itemsPerPage)
+		});
+	});
+}
+
+// actualizacion de datos
+
+function updateUser(req, res) {
+	var userId = req.params.id;
+	var update = req.body;
+
+	//borrar password
+	delete update.password;
+
+	if (userId != req.user.sub) {
+		return res.status(500).send({message: 'No tienes permiso para actualizar los datos'});
+	}
+
+	user.findByIdAndUpdate(userId, update, {new:true}, (err, userUpdate) => {
+		if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+		if (!userUpdate) return res.status(404).send({message: 'No se ah podido actualizar el usuario'});
+
+		return res.status(200).send({user: userUpdate})
+	})
+}
+
+
+
+
 module.exports = {
 	home,
 	test,
 	saveUser,
-	loginUser
+	loginUser,
+	getUser,
+	getUsers,
+	updateUser,
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,12 +2,18 @@
 
 	var express = require('express');
 	var UserController = require('../controllers/user');
+	var md_auth = require('../middleware/authenticated');
 
 	var api = express.Router();
+	var multipart = require('connect-multiparty');
+	var md_upload = multipart({uploadDir: './uploads/users'});
 
 	api.get('/home', UserController.home);
-	api.get('/test', UserController.test);
+	api.get('/test', md_auth.ensureAuth, UserController.test);
 	api.post('/register', UserController.saveUser);
 	api.post('/login', UserController.loginUser);
+	api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
+	api.get('/users/:page?', md_auth.ensureAuth, UserController.home);
+	api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 
 	module.exports = api;
