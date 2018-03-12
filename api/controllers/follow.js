0b7c6538db04a5c75b2c692f5e3ef40322b4050d
luis <luis152288@gmail.com>
2018-03-12 15:44:01 -0400

    listo de usuarios seguidos

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -27,6 +27,57 @@
  		
 	}
 
+	//Dejar de sguir a otro usuario
+
+	function deleteFollow(req, res) {
+		var userId = req.user.sub;
+		var followId = req.params.id;
+
+		follow.find({'user': userId, 'followed':followId}).remove(err => {
+			if(err) return res.status(500).send({message:'Error al Dejar de seguir'});
+
+			return res.status(200).send({message: ' ah dejado de seguir a...'});
+		})
+	}
+
+
+	function getFollowingUsers(req, res) {
+		 var user = req.user.sub;
+
+		 if (req.params.id && req,params.page){
+		 	userId = req.params.id;
+
+		 }
+
+		 var page = 1
+
+		 if (req.params.page) {
+		 	page = req.params.page;
+
+		 }else{
+		 	page = req.params.id;
+
+		 }
+
+		 var itemsPerPage = 4;
+
+		 Follow.find({user:userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err, follows, total) =>{
+		 		if(err) return res.status(500).send({message:'Error en el servidor'});
+
+		 		if (!follows) return res.status(404).send({ message: 'No estas siguiendo a ningun usuario'});
+
+		 		return res.status(200).send({
+		 			total: total,
+		 			pages: Math.ceil(total/itemsPerPage),
+		 			follows
+		 		})
+
+		 });
+	}
+
+
 	module.exports = {
-		saveFollow
+		saveFollow,
+		deleteFollow,
+		getFollowingUsers
 	}
